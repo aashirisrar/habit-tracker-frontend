36509e311d583f13db17c97bfa559d391ab069f3
Aashir Israr <aashirisrar968@gmail.com>
2024-07-22 16:24:32 +0500

    updated the testimonials

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -4,47 +4,99 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <section className="mx-auto flex flex-col gap-8 items-center justify-between py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
-      <div className="flex gap-1">
-        <div>
-          <Star />
-        </div>
-        <div>
-          <Star />
-        </div>
-        <div>
-          <Star />
-        </div>
-        <div>
-          <Star />
-        </div>
-        <div>
-          <Star />
-        </div>
+    <section className="mx-auto flex flex-col gap-14 justify-start py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+      <div className="flex flex-col gap-6">
+        <div className="text-3xl font-bold">Happy Customers</div>
+        <div>Hear what our customer say about us</div>
       </div>
-      <div className="text-lg font-semibold">
-        The app is amazing! It has made buying and selling so much easier for
-        me.
-      </div>
-      <div className="flex items-center justify-center gap-6">
-        <div className="flex gap-5">
-          <div className="h-[50px] w-[50px]">
-            <Image
-              src="/hero.jpg"
-              alt="profile"
-              className="rounded-full w-full h-full"
-              width={50}
-              height={50}
-            />
+      <div className="flex gap-6">
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-1">
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
           </div>
-          <div>
-            <div>John Doe</div>
-            <div>CEO, ABC Company</div>
+          <div className="text-lg font-semibold">
+            The app is amazing! It has made buying and selling so much easier
+            for me.
+          </div>
+          <div className="flex justify-start gap-6">
+            <div className="flex gap-5">
+              <div className="h-[50px] w-[50px]">
+                <Image
+                  src="/hero.jpg"
+                  alt="profile"
+                  className="rounded-full w-full h-full"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <div>
+                <div>John Doe</div>
+                <div>CEO, ABC Company</div>
+              </div>
+            </div>
+            <div className="py-5 outline outline-1"></div>
+            <div className="flex items-center justify-center">
+              <Twitter fill="white" />
+            </div>
           </div>
         </div>
-        <div className="py-5 outline outline-1"></div>
-        <div className="flex items-center justify-center">
-          <Twitter fill="white" />
+        <div className="flex flex-col gap-6">
+          <div className="flex gap-1">
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+            <div>
+              <Star />
+            </div>
+          </div>
+          <div className="text-lg font-semibold">
+            The app is amazing! It has made buying and selling so much easier
+            for me.
+          </div>
+          <div className="flex justify-start gap-6">
+            <div className="flex gap-5">
+              <div className="h-[50px] w-[50px]">
+                <Image
+                  src="/hero.jpg"
+                  alt="profile"
+                  className="rounded-full w-full h-full"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <div>
+                <div>John Doe</div>
+                <div>CEO, ABC Company</div>
+              </div>
+            </div>
+            <div className="py-5 outline outline-1"></div>
+            <div className="flex items-center justify-center">
+              <Twitter fill="white" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
