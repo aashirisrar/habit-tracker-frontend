5e8fb120b29568caec6b89dedc11ba9b11d7c125
Aashir Israr <aashirisrar968@gmail.com>
2024-07-22 14:56:07 +0500

    added homepage components

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,27 @@
-import React from "react";
-const Home = () => {
-  return <div className="App bg-[#040C18]">Hello</div>;
-};
+import Hero from "@/components/hero";
+import KeyFeatures from "@/components/key-features";
+import Potential from "@/components/potential";
+import { MainNavigationMenu } from "@/components/navbar";
+import Discover from "@/components/discover";
+import DownloadNow from "@/components/download-now";
+import Testimonials from "@/components/testimonial";
+import Footer from "@/components/footer";
 
-export default Home;
+export default function Home() {
+  return (
+    <div className="relative flex min-h-screen flex-col bg-background">
+      <MainNavigationMenu />
+      <main className="flex-1">
+        <div className="container relative">
+          <Hero />
+          <KeyFeatures />
+          <Potential />
+          <Discover />
+          <Testimonials />
+          <DownloadNow />
+          <Footer />
+        </div>
+      </main>
+    </div>
+  );
+}
