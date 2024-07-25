94ac085fb0af4d88f23d61f6ebd936e998463c3e
Aashir Israr <aashirisrar968@gmail.com>
2024-07-25 12:01:50 +0500

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token");
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/home/:path*", "/activity/:path*", "/profile/:path*"],
+};
